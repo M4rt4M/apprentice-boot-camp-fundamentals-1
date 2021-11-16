d4f08f9019b2a9e6fd8c99a323a6193bc5182b6a
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-11-16 09:53:12 +0000

    Update cards.js

--- exercises/javascript/cards/cards.js
+++ exercises/javascript/cards/cards.js
@@ -1,98 +1,52 @@
 const PlayingCard = require("./playing-card.js").PlayingCard;
+const Suits = require('./suits').Suits;
 // to execute the runnable code in this file, use the command
 // `node cards/cards.js` from the command line positioned at
 // the project's root directory.
 
-class Cards {
-  getCards() {
-    const result = []
-    const deck = []
-  
-    for (let suit = 0; suit < 4; suit++) {
-      for (let faceValue = 1; faceValue <= 13; faceValue++) {
-        // Create new card
-        const playingCard = new PlayingCard(suit, faceValue);
-        // Add to the deck
-        deck.push(playingCard);
-      }
-    }
-  
-    let cardNumber = 0;
-    for (let card of deck) {
-      let faceValueName;
-      switch (card.faceValue) {
-        case 1:
-          faceValueName = "ace";
-          break;
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-        case 10:
-          faceValueName = card.faceValue.toString();
-          break;
-        case 11:
-          faceValueName = "jack";
-          break;
-        case 12:
-          faceValueName = "queen";
-          break;
-        case 13:
-          faceValueName = "king";
-          break;
-        default:
-          throw new Error(
-            "Something went wrong " +
-              card.faceValue +
-              " is not a valid faceValue!"
-          );
-      }
+class PlayingCardDeck {
+  constructor() {
+    this.cards = this.setCards();
+  }
 
-      let suitName = card.suit;
-      result[cardNumber] = faceValueName + " of " + suitName;
-      cardNumber++;
+  setCards() {
+    var cards = [];
+
+    for (var i = 0; i < Suits.length; i++) {
+      for (var j = 1; j <= 13; j++) {
+        var card = new PlayingCard(Suits[i], j);
+        cards.push(card);
+      }
     }
-  
+
+    return cards;
+  }
+
+  getCards() {
+    var result = [];
+
+    for (var i = 0; i < this.cards.length; i++) {
+      const card = this.cards[i];
+      result.push(card.toString());
+    }
+
     return result;
-  };
+  }
 };
 
-const cards = new Cards();
+const cards = new PlayingCardDeck();
 const deckInOrder = cards.getCards();
 for (const card of deckInOrder) {
   console.log(card);
 }
 
 module.exports = {
-  Cards
+  PlayingCardDeck
 };
 
-// deck: 52x (card: (suit: 0-3), (value: 2-10, J, Q, K, A), push itself to deck)
-
-class Card {
-  constructor(value) {
-    this.value = value;
-    
-  }
-}
-
-const suit = {1: "clubs",
-              2: "diamonds", 
-              3: "hearts", 
-              4: "spades"};
-
-class Deck {
-  constructor(value, suit) {
-    
-  }
 
   // functions: shuffle() {}
   // getRandomCard() {}
-}
 
 /* 
 1 - A
